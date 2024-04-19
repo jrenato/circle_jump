36a33815b53deaf885ed384d2c1411372f130132
Karvalho <renato@karvalho.com.br>
2024-04-19 20:20:00 -0300

    Added trail to jumper

--- Jumper/jumper.gd
+++ Jumper/jumper.gd
@@ -5,6 +5,9 @@ signal captured(area: Area2D)
 var velocity: Vector2 = Vector2(100, 0)
 var jump_speed: int = 1000
 var target: Circle = null
+var trail_length: int = 25
+
+@onready var trail = %Points
 
 
 func _ready() -> void:
@@ -15,17 +18,22 @@ func _process(delta) -> void:
 	pass
 
 
+func _input(event: InputEvent) -> void:
+	if event is InputEventScreenTouch or event is InputEventMouseButton:
+		if target and event.is_pressed():
+			jump()
+
+
 func _physics_process(delta: float) -> void:
 	if target:
 		transform = target.orbit_position.global_transform
 	else:
 		position += velocity * delta
 
-
-func _input(event: InputEvent) -> void:
-	if event is InputEventScreenTouch or event is InputEventMouseButton:
-		if target:
-			jump()
+	# Updated trail points
+	if trail.points.size() > trail_length:
+		trail.remove_point(0)
+	trail.add_point(position)
 
 
 func jump() -> void:
