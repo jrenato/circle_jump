65ad4cacae2f3c79f412879c16fa456f12002019
karvalho <renato@karvalho.com.br>
2024-04-21 08:51:28 -0300

    Improved theme resources

--- Resources/color_scheme.gd
+++ Resources/color_scheme.gd
@@ -1,5 +1,7 @@
 class_name ColorScheme extends Resource
 
+@export var name: String
+@export var unlocked: bool = false
 @export var background: Color
 @export var player_body: Color
 @export var player_trail: Color
