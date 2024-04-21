f8f78d7f55be2f16d1c6d47a8398f5fc3a6722c6
karvalho <renato@karvalho.com.br>
2024-04-21 15:16:04 -0300

    Added default to color scheme options

--- Resources/color_scheme.gd
+++ Resources/color_scheme.gd
@@ -1,6 +1,7 @@
 class_name ColorScheme extends Resource
 
 @export var name: String
+@export var default: bool = false
 @export var unlocked: bool = false
 @export var background: Color
 @export var player_body: Color
