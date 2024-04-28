6633d07fcd3001ea518613323e322b0a05379d65
karvalho <renato@karvalho.com.br>
2024-04-28 14:17:17 -0300

    Improved trail rendering

--- Game/Jumper/jumper.gd
+++ Game/Jumper/jumper.gd
@@ -5,23 +5,45 @@ signal died
 
 var velocity: Vector2 = Vector2(100, 0)
 var jump_speed: int = 1000
-var target: Circle = null
-var trail_length: int = 25
+var target: Circle = null:
+	set(value):
+		target = value
+		is_trail_emitting = target == null
 
-@onready var trail = %Points
+var is_trail_emitting: bool = false
+var trail_length: int = 30
+var trail_points: Array[Vector2]
+var blank_trail: Array[Vector2]
+
 @onready var sprite: Sprite2D = %Sprite2D
+@onready var trail_line: Line2D = %TrailLine2D
 
 
 func _ready() -> void:
 	sprite.material.set_shader_parameter("color", Settings.theme["player_body"])
 	var trail_color: Color = Settings.theme["player_trail"]
-	trail.gradient.set_color(1, trail_color)
+	trail_line.gradient.set_color(0, trail_color)
 	trail_color.a = 0
-	trail.gradient.set_color(0, trail_color)
+	trail_line.gradient.set_color(1, trail_color)
 
 
-func _process(delta) -> void:
-	pass
+func _process(delta: float) -> void:
+	if is_trail_emitting:
+		trail_points.push_front(position)
+	else:
+		if trail_points.size() > 0:
+			blank_trail.push_front(position)
+
+	if trail_points.size() + blank_trail.size() > trail_length:
+		if trail_points.size() > 0:
+			trail_points.pop_back()
+		elif blank_trail.size() > 0:
+			blank_trail.pop_back()
+
+	trail_line.clear_points()
+	
+	for point in trail_points:
+		trail_line.add_point(point)
 
 
 func _physics_process(delta: float) -> void:
@@ -29,11 +51,6 @@ func _physics_process(delta: float) -> void:
 		transform = target.orbit_position.global_transform
 	else:
 		position += velocity * delta
-
-	# Updated trail points
-	if trail.points.size() > trail_length:
-		trail.remove_point(0)
-	trail.add_point(position)
 
 
 func _input(event: InputEvent) -> void:
@@ -46,6 +63,8 @@ func jump() -> void:
 	target.implode()
 	target = null
 	velocity = transform.x * jump_speed
+	trail_points.clear()
+	blank_trail.clear()
 
 	AudioManager.play_sound("Jump")
 
